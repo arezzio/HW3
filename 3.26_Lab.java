3342716af2ce8ba481f480e2e5663ef454e7d565
arezzio <160786245+arezzio@users.noreply.github.com>
2025-10-06 19:28:07 -0500

    Create 3.26_Lab.java

---
+++ 3.26_Lab.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class LabProgram {
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      int inputYear;
+      boolean isLeapYear;
+      
+      isLeapYear = false;
+      inputYear = scnr.nextInt();
+      
+      if (inputYear % 4 == 0) {
+         if (inputYear % 100 == 0) {
+            if (inputYear % 400 == 0) {
+               isLeapYear = true;
+            } else {
+               isLeapYear = false;
+            }
+         } else {
+            isLeapYear = true;
+         }
+      }
+
+      if (isLeapYear) {
+         System.out.println(inputYear + " - leap year");
+      } else {
+         System.out.println(inputYear + " - not a leap year");
+      }
+   }
+}
